0a9352c53393eabe0add85b9a86150ed68ecfd59
aibrohim <ibrohim7840@gmail.com>
2023-09-13 09:09:34 +0000

    changed domain for api.pentagol.site

--- init-letsencrypt.sh
+++ init-letsencrypt.sh
@@ -5,7 +5,7 @@ if ! [ -x "$(command -v docker compose)" ]; then
   exit 1
 fi
 
-domains=(pentagol.site)
+domains=(api.pentagol.site)
 rsa_key_size=4096
 data_path="./data/certbot"
 email="ibrohim7840@gmail.com" # Adding a valid address is strongly recommended
